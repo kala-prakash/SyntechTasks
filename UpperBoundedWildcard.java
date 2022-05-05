5fbac9de9bf6b38fad2275b2ccc2096ded4dabc8
PrakashT <kala.prakash56@gmail.com>
2022-05-05 13:01:50 +0545

    Chore | #182081338 | case study on generic --UpperBounded wildcard

---
+++ UpperBoundedWildcard.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.generics;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author kala
+ */
+public class UpperBoundedWildcard {
+
+    private static double sum(List<? extends Number> list) {
+        double sum = 0.0;
+        for (Number i : list) {
+            sum += i.doubleValue();
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4);
+        System.out.println("The sum of Integer is : " + sum(intList));
+    }
+}
